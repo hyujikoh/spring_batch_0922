bac442033e5653e66296ff6e6eab42889d9d2c80
hyujikoh <dvum0045@gmail.com>
2022-09-26 13:24:34 +0900

    샘플 상품옵션 8개 생성, 각 상품당 4개씩 장바구니

---
+++ src/main/java/com/example/spring_batch_0922/app/cart/service/CartService.java
@@ -0,0 +1,24 @@
+package com.example.spring_batch_0922.app.cart.service;
+
+import com.example.spring_batch_0922.app.cart.entity.CartItem;
+import com.example.spring_batch_0922.app.cart.repository.CartItemRepository;
+import com.example.spring_batch_0922.app.member.entity.Member;
+import com.example.spring_batch_0922.app.product.entity.ProductOption;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+    private final CartItemRepository cartItemRepository;
+
+    public void addItem(Member member, ProductOption productOption, int quantity) {
+        CartItem cartItem = CartItem.builder()
+                .member(member)
+                .productOption(productOption)
+                .quantity(quantity)
+                .build();
+
+        cartItemRepository.save(cartItem);
+    }
+}
--- src/main/java/com/example/spring_batch_0922/app/product/service/ProductService.java
+++ src/main/java/com/example/spring_batch_0922/app/product/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.example.spring_batch_0922.app.product.service;
 
 import com.example.spring_batch_0922.app.product.entity.Product;
+import com.example.spring_batch_0922.app.product.entity.ProductOption;
 import com.example.spring_batch_0922.app.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +14,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public void create(String name, int price, String makerShopName) {
+    public Product create(String name, int price, String makerShopName, List<ProductOption> options){
         Product product = Product.builder()
                 .name(name)
                 .price(price)
@@ -19,5 +22,12 @@ public class ProductService {
                 .build();
 
         productRepository.save(product);
+
+        for ( ProductOption option : options ) {
+            product.addOption(option);
+        }
+
+        productRepository.save(product);
+        return product;
     }
 }
