1791ed39794a05e123be8610155f9cb268b37e2a
hyujikoh <dvum0045@gmail.com>
2022-09-26 13:27:36 +0900

    장바구니에 한 회원이 같은 상품옵션을 2번 이상 넣으면 그 때 부터는 기존 아이템의 수량만 변화

--- src/main/java/com/example/spring_batch_0922/app/cart/repository/CartItemRepository.java
+++ src/main/java/com/example/spring_batch_0922/app/cart/repository/CartItemRepository.java
@@ -3,5 +3,9 @@ package com.example.spring_batch_0922.app.cart.repository;
 import com.example.spring_batch_0922.app.cart.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    Optional<CartItem> findByMemberIdAndProductOptionId(long memberId, long productOptionId);
 }
