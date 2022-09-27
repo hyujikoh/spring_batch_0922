b5667bd9412426ef98791d65a37006b63da78689
hyujikoh <dvum0045@gmail.com>
2022-09-27 09:32:39 +0900

    통계와 정산을 위해, 주문품목에 그 당시 판매가, 도매가 등을 기록

--- src/main/java/com/example/spring_batch_0922/app/order/entity/OrderItem.java
+++ src/main/java/com/example/spring_batch_0922/app/order/entity/OrderItem.java
@@ -29,8 +29,14 @@ public class OrderItem extends BaseEntity {
 
     private int quantity;
 
+    private int price;
+    private int salePrice;
+    private int wholesalePrice;
     public OrderItem(ProductOption productOption, int quantity) {
         this.productOption = productOption;
         this.quantity = quantity;
+        this.price = productOption.getPrice();
+        this.salePrice = productOption.getSalePrice();
+        this.wholesalePrice = productOption.getWholesalePrice();
     }
 }
